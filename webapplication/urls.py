66f7382dfe6a827164266f92f50228e9136ed6bc
Sachin Gupta <70026034+Sachingupta30@users.noreply.github.com>
2021-12-03 16:16:15 +0530

    Update urls.py

--- webapplication/urls.py
+++ webapplication/urls.py
@@ -20,7 +20,7 @@ from crudapplication import views
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('emp', views.emp),
-    path('show', views.show),
+    path('', views.show),
     path('edit/<int:id>', views.edit),
     path('update/<int:id>', views.update),
     path('delete/<int:id>', views.delete),
